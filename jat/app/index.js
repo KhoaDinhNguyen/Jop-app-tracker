060c840ce58df0975864a7b18c85e745f8194c29
Nguyễn Khoa <khoacode1305@gmail.com>
2025-02-13 19:27:17 -0700

    Publishing my first Job Application Tracking App

---
+++ jat/app/index.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { SafeAreaView, ScrollView, View } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+const Home = () => {
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+          <Popularjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
